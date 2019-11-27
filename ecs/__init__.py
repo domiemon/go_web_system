c91c7bed0b35664fa0d59ba2e87b7bbf365ca143
Dmitry Malyshev <dmitry.malyshev@bridgetec.jp>
2019-11-27 21:57:42 +0300

    Deploy to Wercker

--- ecs/__init__.py
+++ ecs/__init__.py
@@ -82,11 +82,18 @@ class ECSService(object):
         :return: the response or raise an Exception
         """
         response = self.describe_service(cluster=cluster, service=service)
+        print response
         running_count = (response.get('services')[0]).get('runningCount')
         task_definition = (response.get('services')[0]).get('taskDefinition')
         desired_count = running_count + delta
-        return self.update_service(cluster=cluster, service=service, taskDefinition=task_definition,
+        try:
+            return self.update_service(cluster=cluster, service=service, taskDefinition=task_definition,
                                    desiredCount=desired_count)
+        except Exception as x:
+            print x
+            raise x
+        # return self.update_service(cluster=cluster, service=service, taskDefinition=task_definition,
+        #                           desiredCount=desired_count)
 
     def update_service(self, cluster, service, taskDefinition, desiredCount=None):
         """
